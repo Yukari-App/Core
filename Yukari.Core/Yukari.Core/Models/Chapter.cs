1df34dc0ceeb8f034ec1f75e2e4b0193448e50a9
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-04 02:18:34 -0300

    feat: add initial models

---
+++ Yukari.Core/Yukari.Core/Models/Chapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yukari.Core.Models
+{
+    internal class Chapter
+    {
+    }
+}
