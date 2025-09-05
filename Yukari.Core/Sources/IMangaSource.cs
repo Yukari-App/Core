58756df0bcc392c3b5418000f52e9da13957378c
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-05 16:58:42 -0300

    refactor: remove unused usings

--- Yukari.Core/Sources/IMangaSource.cs
+++ Yukari.Core/Sources/IMangaSource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Yukari.Core.Sources
+﻿namespace Yukari.Core.Sources
 {
     internal interface IMangaSource
     {
