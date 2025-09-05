a4ae0b6144ea0abc076f6a6699f0acee0ed337b0
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-04 23:34:41 -0300

    refactor: remove unused usings

--- Yukari.Core/Models/Manga.cs
+++ Yukari.Core/Models/Manga.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Yukari.Core.Models
+﻿namespace Yukari.Core.Models
 {
     public record Manga(
             string Id,
