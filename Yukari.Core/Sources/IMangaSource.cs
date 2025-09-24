9ac0c23ade6203088d661da95961f74a42c96c04
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-24 17:05:47 -0300

    feat: add Filters property and change SearchAsync signature

--- Yukari.Core/Sources/IMangaSource.cs
+++ Yukari.Core/Sources/IMangaSource.cs
@@ -6,7 +6,9 @@ namespace Yukari.Core.Sources
     {
         string Name { get; }
 
-        Task<List<Comic>> SearchAsync(string query);
+        IReadOnlyList<Filter> Filters { get; }
+
+        Task<List<Comic>> SearchAsync(string query, Dictionary<string, List<string>> filters);
         Task<List<Comic>> GetTrendingAsync();
         Task<Comic?> GetDetailsAsync(string mangaId);
         Task<List<Chapter>> GetAllChaptersAsync(string mangaId, string language);
