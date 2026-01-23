73339f5364ba454e96ad47d88dbe58d3f9b1da7e
TXG0Fk3 <leovertonx12@outlook.com>
2026-01-23 15:53:16 -0300

    fix(Core): rename mangaId to comicId in method parameters

--- Yukari.Core/Sources/IComicSource.cs
+++ Yukari.Core/Sources/IComicSource.cs
@@ -14,9 +14,9 @@ namespace Yukari.Core.Sources
 
         Task<IReadOnlyList<Comic>> SearchAsync(string query, IReadOnlyDictionary<string, IReadOnlyList<string>> filters, CancellationToken ct = default);
         Task<IReadOnlyList<Comic>> GetTrendingAsync(IReadOnlyDictionary<string, IReadOnlyList<string>> filters, CancellationToken ct = default);
-        Task<Comic?> GetDetailsAsync(string mangaId, CancellationToken ct = default);
+        Task<Comic?> GetDetailsAsync(string comicId, CancellationToken ct = default);
         Task<Chapter?> GetChapterDetailsAsync(string comicId, string chapterId, CancellationToken ct = default);
-        Task<IReadOnlyList<Chapter>> GetAllChaptersAsync(string mangaId, string language, CancellationToken ct = default);
+        Task<IReadOnlyList<Chapter>> GetAllChaptersAsync(string comicId, string language, CancellationToken ct = default);
         Task<IReadOnlyList<ChapterPage>> GetChapterPagesAsync(string chapterId, CancellationToken ct = default);
     }
 }
