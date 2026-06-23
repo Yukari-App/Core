6b1a2b47a1e4a814c753169f5d23f00b53077b8b
TXG0Fk3 <txg0fk3@proton.me>
2026-06-23 04:14:19 -0300

    feat(Sources/Metadata): add ReleasesPage attribute

--- Yukari.Core/Sources/ComicSourceMetadataAttribute.cs
+++ Yukari.Core/Sources/ComicSourceMetadataAttribute.cs
@@ -5,18 +5,21 @@ public sealed class ComicSourceMetadataAttribute : Attribute
 {
     public string Name { get; }
     public string Version { get; }
+    public string? ReleasesPage { get; }
     public string? LogoUrl { get; }
     public string? Description { get; }
 
     public ComicSourceMetadataAttribute(
         string name,
         string version,
+        string? releasesPage = null,
         string? logoUrl = null,
         string? description = null
     )
     {
         Name = name;
         Version = version;
+        ReleasesPage = releasesPage;
         LogoUrl = logoUrl;
         Description = description;
     }
